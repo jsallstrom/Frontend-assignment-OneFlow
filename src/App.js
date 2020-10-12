af2a0afc264ae97d8d2256e4a20bf4d57a3c3dac
Johan Sällström <johansallstrom7@outlook.com>
2020-10-12 14:07:25 +0200

    Some basic uneffective functionality

--- src/App.js
+++ src/App.js
@@ -12,16 +12,23 @@ export default function App() {
 
   const [categories, setCategories] = useState([])
 
-  const [randomJoke, setRandomJoke] = useState("")
+  const [joke, setJoke] = useState("")
 
-  // button that fetches a random joke runs once in the beginning??
+  //const [currentCategory, setCurrentCategory] = useState("")
+
+  const [query, setQuery] = useState("")
+
+  const [searchedJokes, setSearchedJokes] = useState([])
+
+
 
 
   
 
-  const fetchRandomJoke = async () => {
+  const fetchJoke = async () => {
+
     const response = await axios.get("https://api.chucknorris.io/jokes/random")
-    setRandomJoke(response.data.value)
+    setJoke(response.data.value)
     
   }
 
@@ -33,34 +40,106 @@ export default function App() {
 
 
   useEffect(() => {
-    fetchRandomJoke()
+    fetchJoke()
     fetchCategories()
     
     
   }, []);
 
 
+  const fetchCategoryJoke = async (category) => {
+    const response = await axios.get(`https://api.chucknorris.io/jokes/random?category=${category}`)
+    setJoke(response.data.value)
+    
+  }
+
+
+  const fetchSearch = async () => {
+
+    try {
+      const response = await axios.get(`https://api.chucknorris.io/jokes/search?query=${query}`)
+    setSearchedJokes(response.data.result)
+    } catch (error) {
+      console.log(error)
+    }
+
+    
+    
+  }
+
+  const handleSearch = (e) => {
+    e.preventDefault()
+    fetchSearch()
+  }
+
+
+ 
+
+
+
+
   // Display random joke
   // display all categories
+
+
+  // Mainpage, joke + button
+  // Navbar for Categories, easiest if only button-like and generating
+  // joke
+  // same button willl generate new random joke BUT from that new category
+  // usestate(current category)
+  // Searchbar
+  
+
+
+  /**
+   * 
+    listing all the categories
+    navigating into a category and showing a random joke in that category.
+    searching through all the jokes using free text search.
+
+   * 
+
+    BUGS:
+    - when searching "chuck", it doesnt respond until you have loaded everything
+    
+
+
+
+   */
   
 
 
   return (
     <div >
-      {randomJoke}
-      <button onClick={() => fetchRandomJoke()}>Generate new random joke</button>
       <ul>
-            {categories.map((cats, index) => {
+            {categories.map((category, index) => {
           return (
               
 
               <li key={index}>
-                {cats}
+                <button onClick={() => fetchCategoryJoke(category)}>{category}</button>
               </li>
           );
       })}
 
       </ul>
+      {joke}
+      <button onClick={() => fetchJoke()}>Generate new random joke</button>
+      
+      <form onSubmit={handleSearch}>
+        <input type="text" onChange={e=> setQuery(e.target.value)} value={query}></input>
+        <button type="submit">Search</button>
+      </form>
+
+      {searchedJokes.map( joke => {
+        console.log(joke)
+        return (
+            <p key={joke.id}>{joke.value}</p>
+
+        )
+          
+        
+      })}
       
     </div>
   );
